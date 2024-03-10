c199452626062da267e8d69b1b94d7d4ff7370e3
FredzyW <fredrik@wastring.com>
2024-03-10 19:30:26 +0100

    initial commit

---
+++ blocks.def.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"BAT   ", "cat /sys/class/power_supply/BAT0/capacity", 20, 0}
+
+	{"", "date '+%b %d (%a) %I:%M'",		10,		0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
